07dbbaf61785afa60c2e7e42fe9962d14d09f0f1
addison-schwamb <97481978+addison-schwamb@users.noreply.github.com>
2022-09-26 11:43:30 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,115 @@
+import argparse
+import json
+import sys
+import pickle
+from SPM_task import *
+from train_synets import *
+dir = ''
+
+# parse arguments and set parameters for new network
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', type=json.loads)
+args = parser.parse_args()
+kwargs= args.d
+
+def set_all_parameters(alphaR, sigma2, eta, n_train, encoding, seed, damaged_net):
+	params = dict()
+
+	net_params = dict()
+	net_params['N'] = 1000
+	net_params['dt'] = 0.1
+	net_params['alpha'] = 0.1
+	net_params['alphaR'] = alphaR
+	net_params['sigma2'] = sigma2
+	net_params['eta'] = eta
+	net_params['d_input'] = 2
+	net_params['d_output'] = 1
+	params['network'] = net_params
+
+	task_params = dict()
+	t_intervals = dict()
+	t_intervals['fixate_on'], t_intervals['fixate_off'] = 0, 0
+	t_intervals['cue_on'], t_intervals['cue_off'] = 0, 0
+	t_intervals['stim_on'], t_intervals['stim_off'] = 10, 5
+	t_intervals['delay_task'] = 0
+	t_intervals['response'] = 5
+	task_params['time_intervals'] = t_intervals
+	task_params['t_trial'] = sum(t_intervals.values()) + t_intervals['stim_on'] + t_intervals['stim_off']
+	task_params['output_encoding'] = encoding  # how 0, 1, 2 are encoded
+	task_params['keep_perms'] = [(0, 0), (0, 1), (1, 0), (1, 1)]
+	task_params['n_digits'] = 9
+	params['task'] = task_params
+
+	train_params = dict()
+	train_params['n_train'] = int(n_train)  # training steps
+	train_params['n_train_ext'] = int(0)
+	train_params['n_test'] = int(20)	  # test steps
+	params['train'] = train_params
+
+	other_params = dict()
+	other_params['seed'] = seed	 #default is 0
+	other_params['damaged_net'] = damaged_net
+	params['msc'] = other_params
+
+	return params
+
+def get_digits_reps():
+
+	with open('allDigCNNMNIST', 'rb') as f:
+		z_mean, z_log_var, z_sample = pickle.load(f)
+		x_test = pickle.load(f)
+		y_test = pickle.load(f)
+
+	y_test, x_test = np.array(y_test), x_test.reshape([x_test.shape[0], 28, 28])
+
+	return y_test, z_sample
+
+def load_data(name,prefix,dir):
+	filename = prefix + '_' + name
+	with open(dir + filename, 'rb') as f:
+		params, internal_x, x_ICs, r_ICs, error_ratio = pickle.load(f)
+
+	return params, internal_x, x_ICs, r_ICs, error_ratio
+
+def add_input_weights(params):
+	rng = np.random.RandomState(msc_prs['seed'])
+	net_prs = params['network']
+	train_prs = params['train']
+	model_prs = params['model']
+	wi = model_prs['wi']
+	N = model_prs['N']
+
+	if train_prs['init_dist'] == 'Gauss':
+		new_weights = (1. * rng.randn(N, 1)) / net_prs['input_var']
+	elif train_prs['init_dist'] == 'Uniform':
+		new_weights = (2 * rng.rand(N, 1) - 1) / net_prs['input_var']
+
+	new_wi = np.concatenate((new_weights,wi),axis=1)
+	model_prs['wi'] = new_wi
+	net_prs['d_input'] += 1
+	params['model'] = model_prs
+	params['network'] = net_prs
+
+	return params
+
+
+params = set_all_parameters(**kwargs)
+
+# get MNIST digits and set up task
+labels, digits_rep = get_digits_reps()
+net_prs = params['network']
+task_prs = params['task']
+train_prs = params['train']
+msc_prs = params['msc']
+
+task = sum_task_experiment(task_prs['n_digits'], train_prs['n_train'], train_prs['n_train_ext'], train_prs['n_test'], task_prs['time_intervals'],
+						   net_prs['dt'], task_prs['output_encoding'], task_prs['keep_perms'] , digits_rep, labels, msc_prs['seed'])
+exp_mat, target_mat, dummy_mat, input_digits, output_digits = task.experiment()
+
+# load in damaged network
+dmg_params, dmg_x, _, _, _ = load_data(name=msc_prs['damaged_net'],prefix='damaged',dir=dir)
+dmg_params = add_input_weights(dmg_params)
+
+# train new network
+x_train, dmg_x, params = train(params, dmg_params, dmg_x, exp_mat, input_digits)
+x_ICs, r_ICs, internal_x, dmg_x = test(params, dmg_params, x_train, dmg_x, exp_mat, input_digits)
---
+++ remove_neurons.py
@@ -0,0 +1,70 @@
+import argparse
+import json
+import sys
+import pickle
+import numpy as np
+dir = ''
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-d', type=json.loads)
+args = parser.parse_args()
+kwargs= args.d
+
+def load_data(name,prefix,dir):
+	filename = prefix + '_' + name
+	with open(dir + filename, 'rb') as f:
+		params, internal_x, x_ICs, r_ICs, error_ratio = pickle.load(f)
+
+	return params, internal_x, x_ICs, r_ICs, error_ratio
+
+def set_all_parameters(pct_rmv, name):
+	params = dict()
+	params['pct_rmv'] = pct_rmv
+	params['name'] = name
+
+	return params
+
+def save_data_variable_size(*vars1, name=None, prefix='train', dir=None):
+    file_name = prefix + '_' + name
+    with open(dir + file_name, 'wb') as f:
+        pickle.dump((vars1), f, protocol=-1)
+
+init_params = set_all_parameters(**kwargs)
+pct_rmv = init_params['pct_rmv']
+params, internal_x, x_ICs, r_ICs, error_ratio = load_data(name=init_params['name'],prefix='train',dir=dir)
+internal_x = internal_x[:,-1]
+
+xlen = np.size(internal_x)
+model_prs = params['model']
+J = model_prs['J']
+wi = model_prs['wi']
+wo = model_prs['wo']
+wd = model_prs['wd']
+wf = model_prs['wf']
+wfd = model_prs['wfd']
+
+num_to_rmv = round(pct_rmv*xlen)
+rmv_indices = np.random.randint(0,xlen,(num_to_rmv))
+
+for i in range(num_to_rmv):
+	internal_x = np.concatenate((internal_x[0:rmv_indices[i]],internal_x[rmv_indices[i]+1:]))
+	J = np.concatenate((J[0:rmv_indices[i],:],J[rmv_indices[i]+1:,:]),axis=0)
+	J = np.concatenate((J[:,0:rmv_indices[i]],J[:,rmv_indices[i]+1:]),axis=1)
+	wi = np.concatenate((wi[0:rmv_indices[i],:],wi[rmv_indices[i]+1:,:]),axis=0)
+	wo = np.concatenate((wo[0:rmv_indices[i],:],wo[rmv_indices[i]+1:,:]),axis=0)
+	wd = np.concatenate((wd[0:rmv_indices[i],:],wd[rmv_indices[i]+1:,:]),axis=0)
+	wf = np.concatenate((wf[0:rmv_indices[i],:],wf[rmv_indices[i]+1:,:]),axis=0)
+	wfd = np.concatenate((wfd[0:rmv_indices[i],:],wfd[rmv_indices[i]+1:,:]),axis=0)
+
+model_prs['J'] = J
+model_prs['wi'] = wi
+model_prs['wo'] = wo
+model_prs['wd'] = wd
+model_prs['wf'] = wf
+model_prs['wfd'] = wfd
+model_prs['N'] = np.size(internal_x)
+print(model_prs['N'])
+params['model'] = model_prs
+print(np.shape(internal_x))
+
+save_data_variable_size(params, internal_x, x_ICs, r_ICs, error_ratio, name=init_params['name'], prefix='damaged', dir=dir)
